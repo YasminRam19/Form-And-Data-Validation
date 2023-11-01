bcf561543f77a9ae58ab04a5e14a061acca436b9
Yasmin Ramirez <yasmin.ramirez.sanchez19@gmail.com>
2023-11-01 14:50:00 -0600

    State Login moved to a separate component named StateLogin.jsx and the Login.jsx gets user input via Refs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,30 +1,12 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 export default function Login() {
-  const [enteredValues, setEnteredValues] = useState({
-    email: "",
-    password: "",
-  });
-
-  const handleInputChange = (identifier, value) => {
-    setEnteredValues((prevValues) => ({
-      ...prevValues,
-      [identifier]: value,
-    }));
-  };
-  /*const [enteredEmail, setEnteredEmail] = useState("");
-  const [enteredPassword, setEnteredPassword] = useState("");
-
-  const handleEmailChange = (event) => {
-    setEnteredEmail(event.target.value);
-  };
-  const handlePasswordChange = (target) => {
-    setEnteredPassword(event.target.value);
-  };*/
+  const email = useRef("");
+  const password = useRef("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(`User emial: ${enteredEmail}`);
+    console.log(`User emial: ${email.current.value}`);
   };
 
   return (
@@ -34,26 +16,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onChange={(event) => handleInputChange("email", event.target.value)}
-            value={enteredValues.email}
-          />
+          <input id="email" type="email" name="email" ref={email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            onChange={(event) =>
-              handleInputChange("password", event.target.value)
-            }
-            value={enteredValues.password}
-          />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
---
+++ src/components/StateLogin.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (identifier, value) => {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
+  };
+  /*const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+  const handlePasswordChange = (target) => {
+    setEnteredPassword(event.target.value);
+  };*/
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`User emial: ${enteredEmail}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange("email", event.target.value)}
+            value={enteredValues.email}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
+            value={enteredValues.password}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
